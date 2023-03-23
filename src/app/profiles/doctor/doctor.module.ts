9aa42d77ab4c07aa94a8a44a22965bb9e9af1e0c
Abdullah Magdy <magdyabdullah200@gmail.com>
2023-03-24 01:06:57 +0200

    doctor profile done

---
+++ src/app/profiles/doctor/doctor.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfilesRoutingModule } from '../profiles-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { GetDoctorComponent } from './get-doctor/get-doctor.component';
+import { UpdateDoctorComponent } from './update-doctor/update-doctor.component';
+
+@NgModule({
+  declarations: [GetDoctorComponent, UpdateDoctorComponent],
+  imports: [
+    CommonModule,
+    ProfilesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [GetDoctorComponent, UpdateDoctorComponent],
+})
+export class DoctorModule {}
--- src/app/profiles/profiles-routing.module.ts
+++ src/app/profiles/profiles-routing.module.ts
@@ -4,12 +4,16 @@ import { GetPatientComponent } from './patient/get-patient/get-patient.component
 import { UpdatePatientComponent } from './patient/update-patient/update-patient.component';
 import { GetEmployeeComponent } from './empolyee/get-employee/get-employee.component';
 import { UpdateEmployeeComponent } from './empolyee/update-employee/update-employee.component';
+import { GetDoctorComponent } from './doctor/get-doctor/get-doctor.component';
+import { UpdateDoctorComponent } from './doctor/update-doctor/update-doctor.component';
 
 const routes: Routes = [
   { path: 'patient/:id', component: GetPatientComponent },
   { path: 'patient/update/:id', component: UpdatePatientComponent },
   { path: 'employee/:id', component: GetEmployeeComponent },
   { path: 'employee/update/:id', component: UpdateEmployeeComponent },
+  { path: 'doctor/:id', component: GetDoctorComponent },
+  { path: 'employee/update/:id', component: UpdateDoctorComponent },
 ];
 
 @NgModule({
